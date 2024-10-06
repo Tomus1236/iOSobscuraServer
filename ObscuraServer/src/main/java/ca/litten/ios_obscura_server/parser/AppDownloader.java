683b539b72091ca46b73086895bbfd44ecc8de15
UBC Student <chancelp@students.cs.ubc.ca>
2024-10-06 13:07:51 -0700

    itunesmetadata.plist

--- ObscuraServer/src/main/java/ca/litten/ios_obscura_server/parser/AppDownloader.java
+++ ObscuraServer/src/main/java/ca/litten/ios_obscura_server/parser/AppDownloader.java
@@ -20,20 +20,19 @@ public class AppDownloader {
                 System.err.println("Not found");
                 return;
             }
+            String appName = "";
+            String bundleID = "";
+            String version = "0.0";
+            String minimumVersion = "0.0";
             ZipInputStream zipExtractor = new ZipInputStream(connection.getInputStream());
             ZipEntry entry = zipExtractor.getNextEntry();
+            boolean foundOther = false;
             while (entry != null) {
-                if (entry.getName().endsWith("Info.plist")) {
-                    String appName = "";
-                    String bundleID = "";
-                    String version = "0.0";
-                    String minimumVersion = "0.0";
+                if (entry.getName().endsWith(".app/Info.plist")) {
                     byte[] bytes = zipExtractor.readAllBytes();
                     NSDictionary parsedData = (NSDictionary) PropertyListParser.parse(bytes);
                     for (String key : parsedData.allKeys()) {
                         switch (key) {
-                            case "CFBundleDisplayName":
-                                appName = String.valueOf(parsedData.get("CFBundleDisplayName"));
                             case "CFBundleIdentifier":
                                 bundleID = String.valueOf(parsedData.get("CFBundleIdentifier"));
                             case "CFBundleVersion":
@@ -42,16 +41,37 @@ public class AppDownloader {
                                 minimumVersion = String.valueOf(parsedData.get("MinimumOSVersion"));
                         }
                     }
-                    App app = AppList.getAppByBundleID(bundleID);
-                    if (app == null) {
-                        app = new App(appName, bundleID);
-                        AppList.addApp(app);
+                    if (foundOther) {
+                        break;
                     }
-                    app.addAppVersionNoSort(version, new String[]{url.toString()}, minimumVersion);
-                    return;
+                    foundOther = true;
+                }
+                if (entry.getName().endsWith("iTunesMetadata.plist")) {
+                    byte[] bytes = zipExtractor.readAllBytes();
+                    NSDictionary parsedData = (NSDictionary) PropertyListParser.parse(bytes);
+                    for (String key : parsedData.allKeys()) {
+                        switch (key) {
+                            case "softwareVersionBundleId":
+                                bundleID = String.valueOf(parsedData.get("softwareVersionBundleId"));
+                            case "bundleVersion":
+                                version = String.valueOf(parsedData.get("bundleVersion"));
+                            case "itemName":
+                                appName = String.valueOf(parsedData.get("itemName"));
+                        }
+                    }
+                    if (foundOther) {
+                        break;
+                    }
+                    foundOther = true;
                 }
                 entry = zipExtractor.getNextEntry();
             }
+            App app = AppList.getAppByBundleID(bundleID);
+            if (app == null) {
+                app = new App(appName, bundleID);
+                AppList.addApp(app);
+            }
+            app.addAppVersionNoSort(version, new String[]{url.toString()}, minimumVersion);
         } catch (Exception e) {
             System.err.println(e);
         }
