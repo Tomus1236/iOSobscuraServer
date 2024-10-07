da03555c0315e4b45aef146209204f7d5e2b3e1d
UBC Student <chancelp@students.cs.ubc.ca>
2024-10-07 16:49:12 -0700

    done

--- ObscuraServer/src/test/java/ca/ios_obscura_server/backend/AppListTest.java
+++ ObscuraServer/src/test/java/ca/ios_obscura_server/backend/AppListTest.java
@@ -3,7 +3,9 @@ package ca.ios_obscura_server.backend;
 import ca.litten.ios_obscura_server.backend.App;
 import org.junit.*;
 
+import java.util.Arrays;
 import java.util.SortedMap;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -28,19 +30,19 @@ public class AppListTest {
         assertEquals("5.1.1", app.getEarliestSupportedVersion());
         assertEquals("ca.litten.test", app.getBundleID());
         assertEquals("Test app", app.getName());
-        SortedMap<String, String[]> supportedVersions511 = app.getSupportedAppVersions("5.1.1");
-        SortedMap<String, String[]> supportedVersions6 = app.getSupportedAppVersions("6.0");
-        SortedMap<String, String[]> supportedVersions7 = app.getSupportedAppVersions("7.0");
-        assertFalse(supportedVersions511.containsKey("1.0"));
-        assertTrue(supportedVersions6.containsKey("1.0"));
+        String[] supportedVersions511 = app.getSupportedAppVersions("5.1.1");
+        String[] supportedVersions6 = app.getSupportedAppVersions("6.0");
+        String[] supportedVersions7 = app.getSupportedAppVersions("7.0");
+        assertFalse(Arrays.asList(supportedVersions511).contains("1.0"));
+        assertTrue(Arrays.asList(supportedVersions511).contains("1.0"));
         assertArrayEquals(new String[]{"http://litten.ca/test.1.0.3.ipa",
-                "http://backup.litten.ca/test.1.0.3.ipa"}, supportedVersions511.get("1.0.3"));
-        assertEquals(1, supportedVersions511.size());
-        assertArrayEquals(new String[]{"1.0.3"}, supportedVersions511.keySet().toArray());
+                "http://backup.litten.ca/test.1.0.3.ipa"}, app.getUrlsForVersion("1.0.3"));
+        assertEquals(1, supportedVersions511.length);
+        assertArrayEquals(new String[]{"1.0.3"}, supportedVersions511);
         assertArrayEquals(new String[]{"1.0", "1.0.1", "1.0.2", "1.0.3", "1.1"},
-                supportedVersions6.keySet().toArray());
+                supportedVersions6);
         assertArrayEquals(new String[]{"1.0", "1.0.1", "1.0.2", "1.0.3", "1.1", "1.1.1", "1.1.2", "1.1.3"},
-                supportedVersions7.keySet().toArray());
+                supportedVersions7);
         assertTrue(app.showAppForVersion("5.1.1"));
         assertFalse(app.showAppForVersion("5.0"));
     }
--- ObscuraServer/src/test/java/ca/ios_obscura_server/parser/AppDownloaderTest.java
+++ ObscuraServer/src/test/java/ca/ios_obscura_server/parser/AppDownloaderTest.java
@@ -25,8 +25,6 @@ public class AppDownloaderTest {
         assertNotNull(cod);
         assertEquals("2.2.1", cod.getEarliestSupportedVersion());
         assertEquals("Call of Duty: Zombies", cod.getName());
-        assertEquals(url3, cod.getSupportedAppVersions("3.0").get("1.5.0")[0]);
-        assertNull(cod.getSupportedAppVersions("2.2.1").get("1.5.0"));
         assertTrue(AppList.appUrlAlreadyExists(url3));
         assertFalse(AppList.appUrlAlreadyExists("https://archive.org/download/iOSObsc"));
     }
